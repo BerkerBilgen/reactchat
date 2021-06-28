d82aef62689f0da0cb986041f04f461b654f93c6
Berker Bilgen <bilgenmehmetberker@gmail.com>
2021-06-28 09:21:48 +0300

    Updates

---
+++ precache-manifest.95372e997f43c960985c1cadac43bebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a0d75b8ff84638c84b88ba1735d5f6",
+    "url": "/reactchat/index.html"
+  },
+  {
+    "revision": "704ff38b0207f2b25013",
+    "url": "/reactchat/static/css/main.9024d0fa.chunk.css"
+  },
+  {
+    "revision": "665ca037a6fc42694ced",
+    "url": "/reactchat/static/js/2.7b3a5c2a.chunk.js"
+  },
+  {
+    "revision": "3a4abe220cb39419fc765a1c0cd4861e",
+    "url": "/reactchat/static/js/2.7b3a5c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "704ff38b0207f2b25013",
+    "url": "/reactchat/static/js/main.452d8a58.chunk.js"
+  },
+  {
+    "revision": "7e143619a77d4babb8e3",
+    "url": "/reactchat/static/js/runtime-main.e1e04053.js"
+  }
+]);
